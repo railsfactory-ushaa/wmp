c32eefc6b01d7c145aecfa6336536ba419230af4
usha <ushaambig@railsfactory.org>
2014-07-22 10:48:11 +0530

    corrected the code for database operations using sinatra

--- database/database-p2/app2.rb
+++ database/database-p2/app2.rb
@@ -26,20 +26,16 @@ end
 get '/show_tables' do
 db_name = params["database"]
 client2 = Mysql2::Client.new(:host => "localhost", :username => "root", :password => 'root',:database => db_name )
-
-
-@tables= client2.query("show tables;")
- erb :show_tables
-
+@tables = client2.query("show tables;")
+erb :show_tables
 end
+
 get '/show_table' do
 table_name = params["table"]
 db_name = params["database"]
 client3 = Mysql2::Client.new(:host => "localhost", :username => "root", :password => 'root',:database => db_name )
-
 @table = client3.query("Desc #{table_name}")
 @table2 = client3.query("select * from #{table_name}")
-
 erb :show_table
 end
 
